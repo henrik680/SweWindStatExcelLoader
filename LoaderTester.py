74dc1bd3e9f5232b09b2a2885d64f92ad9d85272
henrik <henrik.martensson@gmail.com>
2021-02-08 21:26:24 +0100

    2nd version

---
+++ LoaderTester.py
@@ -0,0 +1,37 @@
+from main import *
+
+
+
+def run(request):
+    print("Starting SweWindStatExcelLoader")
+    file_name = ''
+    file_name = '/Users/henrik/data/Godkända anläggningar_14cols50rows.xlsx'
+    if file_name is not '':
+        logging.info("Excel File=" + file_name)
+        df = openExcel(file_name)
+    else:
+        fileUrl = "http://epi6.energimyndigheten.se/SharePoint/Eugen/Godkända anläggningar.xlsx"
+        logging.info("URL=" + fileUrl)
+        df = openExcelUrl(fileUrl)
+
+    print(df.to_csv(index=False))
+    csvString = remove_mid_newlines(df.dropna(axis='columns').to_csv(index=False))
+    print("###{}###".format(csvString))
+
+
+
+def test_remove_mid_newline():
+    csv = """Anläggningens namn,Organisationsnr,Företagsnamn,Innehav,Tilldelningsfaktor (%),Vid ansökan angiven normalårsproduktion (MWh),Förväntad elcertifikatberättigad produktion (MWh),Installerad effekt (kW),Energikälla,Typ,Nätområdes-ID,Elområde,"Slutdatum 
+för tilldelning","Vid ansökan angivet
+drifttagningsdatum produktionsenhet (vid flera enheter anges drifttagningsdatum för första och sista enhet)"
+Privatperson,Privatperson,Privatperson,1 ägare,100.0,45,45,42.0,Sol,Sol,IKN,SE4,2030-07-07,2015-06-10 - 2015-06-10
+Bergeforsen kraftstation,556044-8887,Bergeforsens Kraft AB,1 ägare,1.44,735000,10584,144000.0,Vatten,Vatten,SUR,SE2,2026-09-01,1955-10-07 - 1959-12-01
+Privatperson,Privatperson,Privatperson,1 ägare,100.0,49,49,49.4,Sol,Sol,IKN,SE3,2027-02-06,2012-01-24 - 2016-04-22
+Väby 1,556857-0591,Väby Driftintressenter AB,1 ägare,100.0,5400,5400,2000.0,Vind,Landbaserad,SYD,SE4,2026-12-27,2011-12-22 - 2011-12-22
+Väby 2,556857-0591,Väby Driftintressenter AB,1 ägare,100.0,5400,5400,2000.0,Vind,Landbaserad,SYD,SE4,2027-02-08,2012-01-19 - 2012-01-19
+"""
+    print(remove_mid_newlines(csv))
+
+
+#run(123)
+test_remove_mid_newline()
--- main.py
+++ main.py
@@ -1,30 +1,81 @@
 import pandas as pd
+import re
 import requests
+import logging
 from google.cloud import storage
 from google.cloud.storage import Blob
 
 def openExcelUrl(url):
     s = requests.get(url).content
-    xl = pd.read_excel(s, skiprows=4, engine='openpyxl')
-    return xl
+    return openExcel(s)
+
+def openExcel(file_or_content):
+    df = pd.read_excel(file_or_content,
+                       engine='openpyxl',
+                       skiprows=4)
+    logging.debug("openExcel(...): pandas dataframe from excel top 10:{}".format(df.head(10)))
+    return df
+
+
+def legacy_remove_mid_newlines(csv):
+    # Swedish renewable energy excel file has new line in the middle of column headers
+
+    iter_max = 1
+    first_pos = csv.find('"')
+    if first_pos > 0: found=True
+    new_csv = csv[:first_pos]
+    while found > 0 and iter_max < 100:
+        second_pos = csv[first_pos+1:].find('"') + first_pos+1
+        new_csv += csv[first_pos:second_pos+2].replace('\n','')
+        first_pos = csv[second_pos+1:].find('"') + second_pos+1
+        if first_pos==second_pos: found=False
+        iter_max += 1
+    new_csv += csv[second_pos+1:]
+    return new_csv
+
+def remove_mid_newlines(csv):
+    # Swedish renewable energy excel file has new line in the middle of column headers
+    (csv_new,csv_new_array,latest_end) = ('',[],0)
+    for match in re.compile('\"(.|\n)*?\"').finditer(csv):   # non greedy over carriage return line feed
+        latest_start = match.start()
+        csv_new_array.append(csv[latest_end:latest_start])
+        csv_new_array.append(match.group().replace('\n',''))
+        latest_end = match.end()
+    csv_new_array.append(csv[latest_end:])
+    for s in csv_new_array:
+        csv_new += s
+    return csv_new
 
 
 def upload_blob_string(bucket_name, csvString, destination_blob_name):
     client = storage.Client()
     bucket = client.get_bucket(bucket_name)
     blob = Blob(destination_blob_name, bucket)
-    blob.upload_from_string(
+    return blob.upload_from_string(
         data=csvString,
         content_type='text/csv')
 
 
 def run(request):
     print("Starting SweWindStatExcelLoader")
-    fileUrl = "http://epi6.energimyndigheten.se/SharePoint/Eugen/Godkända anläggningar.xlsx"
-    print("URL=" + fileUrl)
-    df = openExcelUrl(fileUrl)
-    csvString = df.to_csv()
+    file_name = ''
+    file_name = '/Users/henrik/data/Godkända anläggningar_14cols50rows.xlsx'
+    #file_name = '/Users/henrik/data/Godkända anläggningar_5cols15rows.xlsx'
+    #file_name = '/Users/henrik/data/Godkända anläggningar_2cols3rows.xlsx'
+    if file_name is not '':
+        logging.info("Excel File=" + file_name)
+        df = openExcel(file_name)
+    else:
+        fileUrl = "http://epi6.energimyndigheten.se/SharePoint/Eugen/Godkända anläggningar.xlsx"
+        logging.info("URL=" + fileUrl)
+        df = openExcelUrl(fileUrl)
+    csvString = remove_mid_newlines(df.dropna(axis='columns').to_csv(index=False)) # Dropping empty column
     bucket_name = "swe-renew-energy-stat"
     destination_blob_name = "blob-csv"
     upload_blob_string(bucket_name, csvString, destination_blob_name)
-    print("Uploaded {} to bucket {} and {}".format(df.size,bucket_name,destination_blob_name))
+    print("Uploaded size={} to bucket {} and {}".format(df.size,bucket_name,destination_blob_name))
+
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run(234)
