c0e063acd238f3f260dcd39553d5fa42d2eadd36
henrik <henrik.martensson@gmail.com>
2021-02-09 08:10:42 +0100

    Cleaning up. Promoted regex handling of new line in the middle of column.

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ import logging
 from google.cloud import storage
 from google.cloud.storage import Blob
 
+
+fileUrl = "http://epi6.energimyndigheten.se/SharePoint/Eugen/Godkända anläggningar.xlsx"
+bucket_name = "swe-renew-energy-stat"
+destination_blob_name = "blob-csv"
+
+
 def openExcelUrl(url):
     s = requests.get(url).content
     return openExcel(s)
+
 
 def openExcel(file_or_content):
     df = pd.read_excel(file_or_content,
@@ -16,22 +23,6 @@ def openExcel(file_or_content):
     logging.debug("openExcel(...): pandas dataframe from excel top 10:{}".format(df.head(10)))
     return df
 
-
-def legacy_remove_mid_newlines(csv):
-    # Swedish renewable energy excel file has new line in the middle of column headers
-
-    iter_max = 1
-    first_pos = csv.find('"')
-    if first_pos > 0: found=True
-    new_csv = csv[:first_pos]
-    while found > 0 and iter_max < 100:
-        second_pos = csv[first_pos+1:].find('"') + first_pos+1
-        new_csv += csv[first_pos:second_pos+2].replace('\n','')
-        first_pos = csv[second_pos+1:].find('"') + second_pos+1
-        if first_pos==second_pos: found=False
-        iter_max += 1
-    new_csv += csv[second_pos+1:]
-    return new_csv
 
 def remove_mid_newlines(csv):
     # Swedish renewable energy excel file has new line in the middle of column headers
@@ -60,22 +51,16 @@ def run(request):
     logging.info("Starting SweWindStatExcelLoader")
     file_name = ''
     #file_name = '/Users/henrik/data/Godkända anläggningar_14cols50rows.xlsx'
-    #file_name = '/Users/henrik/data/Godkända anläggningar_5cols15rows.xlsx'
-    #file_name = '/Users/henrik/data/Godkända anläggningar_2cols3rows.xlsx'
     if file_name is not '':
         logging.info("Excel File=" + file_name)
         df = openExcel(file_name)
     else:
-        fileUrl = "http://epi6.energimyndigheten.se/SharePoint/Eugen/Godkända anläggningar.xlsx"
         logging.info("URL=" + fileUrl)
         df = openExcelUrl(fileUrl)
-    csvString = remove_mid_newlines(df.dropna(axis='columns').to_csv(index=False)) # Dropping empty column
-    bucket_name = "swe-renew-energy-stat"
-    destination_blob_name = "blob-csv"
+    csvString = remove_mid_newlines(df.to_csv(index=False, sep=';')) # Dropping empty column
     upload_blob_string(bucket_name, csvString, destination_blob_name)
     logging.info("Uploaded size={} to bucket {} and {}".format(df.size,bucket_name,destination_blob_name))
 
 
 if __name__ == '__main__':
-  logging.getLogger().setLevel(logging.INFO)
   run(234)
